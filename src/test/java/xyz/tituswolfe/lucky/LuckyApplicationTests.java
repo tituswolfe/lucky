7f560202f18a99cc4bb0d579e8abdc7d575c778d
Titus Wolfe <titusawolfe@gmail.com>
2026-06-10 16:58:32 -0500

    Initial commit

---
+++ src/test/java/xyz/tituswolfe/lucky/LuckyApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tituswolfe.lucky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
